995e3ec6f11f7c7bba5c385a39c87b331a8b6234
Nklj <vlkvnklj@gmail.com>
2022-02-08 21:56:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, NavLink as Link } from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" activeClassName="active" >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="dashboard" activeClassName="active">
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link to="about" activeClassName="active">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="dashboard/*" element={<Dashboard />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
+
+export const Home = () => {
+  return <div>You are in Home page</div>;
+};
+export const About = () => {
+  return <div>This is the page where you put details about yourself</div>;
+};
+export const NotFound = () => {
+  return <div>This is a 404 page</div>;
+};
 
 export default App;
